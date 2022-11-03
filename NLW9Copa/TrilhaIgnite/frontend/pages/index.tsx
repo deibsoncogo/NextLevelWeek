61423f145c858c998e31030a4d6ef3c60c3efe2f
Deibson Cogo <deibsoncogo@outlook.com>
2022-11-03 14:40:40 -0300

    🎉 feat <frontend>: implementação do getServerSideProps

--- NLW9Copa/TrilhaIgnite/frontend/pages/index.tsx
+++ NLW9Copa/TrilhaIgnite/frontend/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+interface IProps {
+  count: number
+}
+
+export default function Home(props: IProps) {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello word</h1>
+      <h2>Contagem: {props.count}</h2>
+    </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return { props: { count: data.count } }
 }
