f194f7339ed0b7ddb66c11c81688ddce9a96dda0
Deibson Cogo <deibsoncogo@outlook.com>
2022-11-03 19:32:08 -0300

    🎉 feat <mobile>: implementando a dependência native base

--- NLW9Copa/TrilhaIgnite/mobile/App.tsx
+++ NLW9Copa/TrilhaIgnite/mobile/App.tsx
@@ -1,28 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, Text, Center } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello React Native</Text>
+      <Center flex='1' bgColor='black'>
+        <Text color='white' fontSize={24}>
+          Hello React Native
+        </Text>
+
         <StatusBar style="auto" />
-      </View>
+      </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#ffffff',
-    fontSize: 24,
-  }
-});
