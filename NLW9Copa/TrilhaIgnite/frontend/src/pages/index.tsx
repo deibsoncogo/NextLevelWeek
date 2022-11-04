7c4a917b2ea1c84e39960f65f6888967e5f02965
Deibson Cogo <deibsoncogo@outlook.com>
2022-11-04 20:19:51 -0300

    ✨ feat <frontend>: criação da página home de forma estática

--- NLW9Copa/TrilhaIgnite/frontend/src/pages/index.tsx
+++ NLW9Copa/TrilhaIgnite/frontend/src/pages/index.tsx
@@ -1,12 +1,75 @@
-interface IProps {
-  count: number
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import iconCheckImg from '../assets/icon-check.svg'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+
+interface Props {
+  poolCount: number
 }
 
-export default function Home(props: IProps) {
+export default function Home({ poolCount }: Props) {
   return (
-    <div className='text-violet-500 font-bold text-4xl'>
-      <h1>Hello word</h1>
-      <h2>Contagem: {props.count}</h2>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+      <main>
+        <Image src={logoImg} alt='NLW 9 Copa' />
+
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className='mt-10 flex items-center gap-2'>
+          <Image src={usersAvatarExampleImg} alt='usersAvatarExampleImg' />
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-green'>+ 12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form className='mt-10 flex gap-2'>
+          <input
+            type='text'
+            required
+            placeholder='Qual nome do seu bolão?'
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+          />
+
+          <button
+            type='submit'
+            // eslint-disable-next-line max-len
+            className='bg-ignite-yellow1 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-ignite-yellow2'
+          >
+            CRIAR MEU BOLÃO
+          </button>
+        </form>
+
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas
+        </p>
+
+        <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt='iconCheckImg' />
+
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+ {poolCount}</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+
+          <div className='w-px h-14 bg-gray-600' />
+
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt='iconCheckImg' />
+
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+ 2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image src={appPreviewImg} alt='appPreviewImg' quality={100} />
     </div>
   )
 }
@@ -15,5 +78,5 @@ export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count')
   const data = await response.json()
 
-  return { props: { count: data.count } }
+  return { props: { poolCount: data.count } }
 }
--- NLW9Copa/TrilhaIgnite/frontend/tailwind.config.js
+++ NLW9Copa/TrilhaIgnite/frontend/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { sans: 'Roboto, sans-serif' },
-      colors: { gray: { 900: '#121214' } },
+      backgroundImage: { app: 'url(/app-bg.png)' },
+      colors: {
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+        },
+        ignite: {
+          green: '#129E57',
+          yellow1: '#F7FF43',
+          yellow2: '#E5CD3D',
+        },
+      },
     },
   },
   plugins: [],
