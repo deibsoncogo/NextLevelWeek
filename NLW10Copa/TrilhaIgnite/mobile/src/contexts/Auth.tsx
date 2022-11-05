ffc2f9cc2e0fa7ce2d63f6fcf7a77413b98494f2
Deibson Cogo <deibsoncogo@outlook.com>
2022-11-05 14:56:16 -0300

    🔒️ feat <mobile>: criação da autenticação

--- NLW10Copa/TrilhaIgnite/mobile/src/contexts/Auth.tsx
+++ NLW10Copa/TrilhaIgnite/mobile/src/contexts/Auth.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import * as AuthSession from 'expo-auth-session'
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface IUserProps {
   name: string
@@ -7,6 +12,7 @@ interface IUserProps {
 
 export interface IAuthContextProps {
   user: IUserProps
+  isUserLoading: boolean
   signIn: () => Promise<void>
 }
 
@@ -17,14 +23,42 @@ interface IAuthContextProvider {
 }
 
 export function AuthContextProvider({ children }: IAuthContextProvider) {
+  const [isUserLoading, setIsUserLoading] = useState<boolean>(false)
+  const [user, setUser] = useState<IUserProps>({} as IUserProps)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '611709203884-njqclfuv5hjf65e708n9s88mk8fuod5u.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  })
+
   async function signIn() {
-    console.log('entrou na função signIn')
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.error('error =>', error)
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(accessToken: string) {
+    console.log('Token de autenticação =>', accessToken)
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
+      user,
+      isUserLoading,
       signIn,
-      user: { name: 'Dev Primeiro', avatarUrl: 'https://github.com/devprimeiro.png' },
     }}
     >
       {children}
--- NLW10Copa/TrilhaIgnite/mobile/src/screens/SignIn.tsx
+++ NLW10Copa/TrilhaIgnite/mobile/src/screens/SignIn.tsx
@@ -2,8 +2,11 @@ import { Fontisto } from '@expo/vector-icons'
 import { Center, Icon, Text } from 'native-base'
 import Logo from '../assets/logo.svg'
 import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
   return (
     <Center flex={1} bgColor='gray.900' p={7}>
       <Logo />
@@ -13,6 +16,7 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
         type='secondary'
         mt={12}
+        onPress={signIn}
       />
 
       <Text color='white' textAlign='center' mt={4}>
